c01856e54d1e7ea30367ff082c4233ee6b17c636
alexovonathan22 <avo.nathan@gmail.com>
2019-12-10 15:59:47 +0100

    add two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-    <div>
-      <h1> {{ pageTitle }} </h1>
-      <pm-products></pm-products>
+    <br/>
+    <div class="container"> 
+      <div>
+        <h1> {{ pageTitle }} </h1>
+        <pm-products></pm-products>
+      </div>
     </div>
   `
 })
